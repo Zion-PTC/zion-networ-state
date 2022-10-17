bbe80950c2c5f10576f62b000e7baa19167d431f
Arianna Minucci <arianna.minucci@me.com>
2022-10-17 13:15:21 +0200

    Recompiled contracts

--- apps/scripts/src/Testpack/testpack.js
+++
@@ -1,3 +0,0 @@
-import { create } from "@zionstate/testpack/create";
-import { awesomet } from "@zionstate/testpack/awesomet";
-import { bea } from "@zionstate/testpack/bea";
--- apps/scripts/src/index.js
+++
@@ -1,5 +0,0 @@
-import { random, ciao, nuovo, arianna } from "testpack";
-console.log(random);
-console.log(ciao);
-console.log(nuovo);
-console.log(arianna);
