2d3d740539f0a5fd665abffa2bedbaeebef0938c
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-10-21 18:45:23 +0200

    aggiunto id a MDContent
    sistemare

--- apps/nft-minter/src/components/MDContent.tsx
+++ apps/nft-minter/src/components/MDContent.tsx
@@ -25,5 +25,16 @@ const MDContentArea = styled.div`
 
 export default function MDContent(props: { contentHtml: string }) {
   const { contentHtml } = props;
-  return <MDContentArea dangerouslySetInnerHTML={{ __html: contentHtml }} />;
+  return (
+    <MDContentArea
+      dangerouslySetInnerHTML={{ __html: contentHtml }}
+      // TODO #3 per non far buggare il render ho aggiunto un
+      // id a questo elemento con lo stesso id che viene
+      // ricercato in _app. Al momento cerca un elemento con
+      // id "profile-area", ma questa cosa sarebbe da
+      // cambiare, probabilmente la miglior cosa è dargli un
+      // nome più generico
+      id="profile-area"
+    />
+  );
 }
