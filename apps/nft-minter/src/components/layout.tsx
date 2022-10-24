92e5db587399d56c3034250abdbcb16c894d6a8c
ariannatnl <arianna.minucci@me.com>
2022-10-24 18:13:25 +0200

    casino che ho fatto

--- apps/nft-minter/src/components/layout.tsx
+++ apps/nft-minter/src/components/layout.tsx
@@ -4,17 +4,6 @@ import { HTML } from "@zionstate/ui";
 import { ILayout } from "./Types/index";
 import styled from "styled-components";
 
-const ARROWLEFT =
-  "https://ipfs.io/ipfs/QmY65zzivWgvu7KHCtghz6ZaUtXLLtoehPk4nMHuLgWjEC?filename=arrowleft_white.png";
-const ARROWRIGHT =
-  "https://ipfs.io/ipfs/QmRjd7bcAef7Pfg1Go1KXna68FmFLBPzQ5X1hnkt9rbVP3?filename=arrowright_white.png";
-const HOME =
-  "https://ipfs.io/ipfs/QmZdkHiWkE9J9RTJDfAX6oQ7BtcuHVdtgGSUv6edLqwn68?filename=homepage_white.png";
-const CUORE =
-  "https://ipfs.io/ipfs/QmdNx8VYWL8C4e1qFNCy5UWpstrtQ3sHLgF2dsFkD7En9i?filename=cuore.png";
-const ACCOUNT =
-  "https://ipfs.io/ipfs/QmR7uMi2vxb1VAqfb1Ex8k754A5XngnCZ82hc6m9dQt8dv?filename=utente.png";
-
 const components = HTML.React.components;
 // Components
 const ConnectionButton = components.Elements.ButtonTypes.ConnectionButton;
@@ -30,23 +19,50 @@ const Home = <Link href="/">Home</Link>;
 const ConnectWallet = <Link href="/connect-wallet">Connect Wallet</Link>;
 const Collection = <Link href="/collection">Collection</Link>;
 
+///FOOTER LINKS///////////////////////////////////////////////////////////////////////////////////////
+
+const ARROWLEFT =
+  "https://ipfs.io/ipfs/QmY65zzivWgvu7KHCtghz6ZaUtXLLtoehPk4nMHuLgWjEC?filename=arrowleft_white.png";
+const ARROWRIGHT =
+  "https://ipfs.io/ipfs/QmRjd7bcAef7Pfg1Go1KXna68FmFLBPzQ5X1hnkt9rbVP3?filename=arrowright_white.png";
+////const HOME =
+////  "https://ipfs.io/ipfs/QmZdkHiWkE9J9RTJDfAX6oQ7BtcuHVdtgGSUv6edLqwn68?filename=homepage_white.png";
+const CUORE =
+  "https://ipfs.io/ipfs/QmdNx8VYWL8C4e1qFNCy5UWpstrtQ3sHLgF2dsFkD7En9i?filename=cuore.png";
+const ACCOUNT =
+  "https://ipfs.io/ipfs/QmR7uMi2vxb1VAqfb1Ex8k754A5XngnCZ82hc6m9dQt8dv?filename=utente.png";
+
+class FooterLink {
+  src;
+  link;
+  constructor(link, src) {
+    this.link = link;
+    this.src = src;
+  }
+}
+
+const HOME = new FooterLink(
+  "https://ipfs.io/ipfs/QmZdkHiWkE9J9RTJDfAX6oQ7BtcuHVdtgGSUv6edLqwn68?filename=homepage_white.png",
+  Home
+);
 const AIconsFooter = (props: { icons: string[] }) => {
   return (
     <>
-      {props.icons.map((icon, idx) => (
-        <AIconFooter src={icon} key={idx} />
+      {props.icons.map((icon, idx, link) => (
+        <AIconFooter src={icon} key={idx} link={link} />
       ))}
     </>
   );
 };
 
-const AIconFooter = (props: { src: string }) => {
+const AIconFooter = (props: { src: string; link: string }) => {
   return (
     <a>
       <IconaFooter src={props.src} />
     </a>
   );
 };
+const ICONS = [ARROWLEFT, ARROWRIGHT, HOME, CUORE, ACCOUNT];
 
 const IconaFooter = styled.img`
   width: 35px;
@@ -55,6 +71,8 @@ const IconaFooter = styled.img`
   margin: 0;
   cursor: pointer;
 `;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const Main = styled.main`
   overflow: auto;
@@ -74,17 +92,12 @@ const Logo = styled.img`
   height: 60px;
   border-radius: 100%;
   place-self: center;
-  -webkit-box-shadow: 0px 0px 1px 0.1px #8a8a8a;
-  -moz-box-shadow: 0px 0px 1px 0.1px #8a8a8a;
-  -o-box-shadow: 0px 0px 1px 0.1px #8a8a8a;
-  box-shadow: 0px 0px 1px 0.1px #8a8a8a;
   overflow: hidden;
   transition-duration: 0.8s;
   transition-property: transform;
   &:hover {
     transform: rotate(360deg);
   }
-
   @keyframes ruota {
     from {
       top: 0px;
@@ -109,8 +122,6 @@ const Layout: ILayout = function ({
   showButton,
   theme,
 }) {
-  const ICONS = [ARROWLEFT, ARROWRIGHT, HOME, CUORE, ACCOUNT];
-
   return (
     <>
       <NavBar ref={navbar}>
