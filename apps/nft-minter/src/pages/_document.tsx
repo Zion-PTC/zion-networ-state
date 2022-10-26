00d57a2a949b4c156f21e905f978f23bd2f609df
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-10-26 19:32:08 +0200

    favicon fail

--- apps/nft-minter/src/pages/_document.tsx
+++ apps/nft-minter/src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,500;1,600;1,700;1,900&display=swap"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
