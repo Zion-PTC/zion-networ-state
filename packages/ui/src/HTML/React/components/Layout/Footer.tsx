a30f5715b70546cdec788c9e649f8ae09618a7bd
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-10-26 19:31:04 +0200

    height and bordertop

--- packages/ui/src/HTML/React/components/Layout/Footer.tsx
+++ packages/ui/src/HTML/React/components/Layout/Footer.tsx
@@ -2,17 +2,16 @@ import styled from "styled-components";
 import { FooterProps } from "./Types";
 
 export const Footer = styled.footer<FooterProps>`
-  position: fixed;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-template-areas: "freccias Frecciad home cuore account";
   text-align: center;
   bottom: 0;
-  min-height: ${(props) => (props.minHeight ? props.minHeight : "4rem")};
   width: 100%;
+  height: 100%;
   z-index: 2;
   background-color: black;
-  border-color: 1px solid white;
+  border-top: 1px solid white;
   color: white;
   place-items: center;
 `;
