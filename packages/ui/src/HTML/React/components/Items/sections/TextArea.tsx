c0ea12de397ffe370552326c38844572df369721
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-10-26 19:29:55 +0200

    added 100%

--- packages/ui/src/HTML/React/components/Items/sections/TextArea.tsx
+++ packages/ui/src/HTML/React/components/Items/sections/TextArea.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const TextArea = styled.div<{ gridArea: string }>`
+export const TextArea = styled.div<{ gridArea?: string }>`
+  width: 100%;
   height: 60%;
   background-color: grey;
   grid-area: ${(props) => props.gridArea};
