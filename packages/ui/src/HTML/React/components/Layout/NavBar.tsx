8704ab8efc5e2b0552077e48d9c1f44352c910d8
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-10-26 19:31:25 +0200

    added templCol and areas in props

--- packages/ui/src/HTML/React/components/Layout/NavBar.tsx
+++ packages/ui/src/HTML/React/components/Layout/NavBar.tsx
@@ -4,8 +4,11 @@ import { NavBarProps } from "./Types";
 export const NavBar = styled.nav<NavBarProps>`
   top: 0;
   display: grid;
-  min-height: ${(props) => (props.minHeight ? props.minHeight : "4rem")};
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-areas: "home . logo web3 theme";
   text-align: center;
+  ${(props) =>
+    props.gridTemplateColumns &&
+    `grid-template-columns: ${props.gridTemplateColumns};`}
+  ${(props) =>
+    props.gridTemplateAreas &&
+    `grid-template-areas: ${props.gridTemplateAreas};`}
 `;
