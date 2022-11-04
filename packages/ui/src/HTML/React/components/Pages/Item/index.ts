4e7328dc3fd7daf93bee3798467b930c7dcde98a
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-04 15:48:51 +0100

    + itemPage v4

---
+++ packages/ui/src/HTML/React/components/Pages/Item/index.ts
@@ -0,0 +1,19 @@
+import {
+  ItemProps_v1 as v1Props,
+  Item_v1 as v1,
+} from "./Item_v1";
+
+import {
+  Item_v2Props as v2Props,
+  Item_v2 as v2,
+} from "./Item_v2";
+
+export type ItemProps_v1 = v1Props;
+export const Item_v1 = v1;
+export type Item_v2Props = v2Props;
+export const Item_v2 = v2;
+
+////
+export type ItemProps = v2Props;
+
+export const Item = v2;
