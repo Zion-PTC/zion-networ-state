769009bcb26baf0e3d5a668dd20ba5a8e1aa9642
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-05 13:45:30 +0100

    created _document for UI app

---
+++ packages/ui/src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+import { robotoGoogleQuery } from "../HTML/React/style/Global";
+
+function Document_v2() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href={robotoGoogleQuery} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export const Document = Document_v2;
+
+export default Document;
