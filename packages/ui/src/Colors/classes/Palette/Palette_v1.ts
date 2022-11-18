07877f7707a39e73862950b3f804b3a944cbec5c
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-18 18:07:53 +0100

    Colors

--- packages/ui/src/Colors/classes/Palette/Palette_v1.ts
+++ packages/ui/src/Colors/classes/Palette/Palette_v1.ts
@@ -69,3 +69,40 @@ export class Palette_v1 {
   smoke?: string;
   constructor() {}
 }
+
+/////////OLD
+/////////PALETTE
+type Palette5Colors = {
+  color1: string;
+  color2: string;
+  color3: string;
+  color4: string;
+  color5: string;
+};
+type Palette8Colors = {
+  color1: string;
+  color2: string;
+  color3: string;
+  color4: string;
+  color5: string;
+  color6: string;
+  color7: string;
+  color8: string;
+};
+
+type ColorPaletteTypes = "hex";
+
+export interface ColorPalette {
+  __type: ColorPaletteTypes;
+  colors: Palette5Colors | Palette8Colors;
+}
+
+export class ColorPalette {
+  constructor(
+    type: ColorPaletteTypes,
+    colors: Palette5Colors | Palette8Colors
+  ) {
+    this.__type = type;
+    this.colors = colors;
+  }
+}
