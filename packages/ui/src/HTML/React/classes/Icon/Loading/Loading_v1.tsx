5ebf946ad90c1d6746ebfce7bff609f6cb2fcfa6
ariannatnl <arianna.minucci@me.com>
2022-11-19 14:31:19 +0100

    loading mmm

--- packages/ui/src/HTML/React/classes/Icon/Loading/Loading_v1.tsx
+++ packages/ui/src/HTML/React/classes/Icon/Loading/Loading_v1.tsx
@@ -1,7 +1,42 @@
-export type Loading_v1Data = "Loading_v1Data";
-export type Loading_v1Booleans = "Loading_v1Booleans";
-export type Loading_v1Props = "Loading_v1Props";
-export type Loading_v1ClassProps = "Loading_v1ClassProps";
-export type Loading_v1AsChild = "Loading_v1AsChild";
+import styled from "styled-components";
 
-export const Loading_v1 = "Loading_v1";
+/////// TYPES
+export type Loading_v1Data = { type: "waves" };
+
+export type Loading_v1Booleans = { display: boolean };
+
+export type Loading_v1Props = NoizProps<
+  Loading_v1Data & Loading_v1Booleans & utility.CssStyled
+>;
+
+export type Loading_v1ClassProps =
+  NoizDatas<Loading_v1Props>;
+
+export type Loading_v1AsChild = MakeAsChild<
+  "Loading",
+  Loading_v1ClassProps
+>;
+/////////////
+
+////////CLASS
+export class Loading_v1 extends BaseNoiz<
+  Loading_v1Data,
+  Loading_v1Booleans
+> {
+  constructor(props: Loading_v1ClassProps) {
+    super(props);
+  }
+  Html = (props: Loading_v1Props) => {
+    return (
+      <div className={props.className}>{props.age}</div>
+    );
+  };
+
+  Style = styled(this.Html)``;
+
+  Mapper = Loading_v1.mapperFactory(this.Style);
+
+  render() {
+    return <this.Mapper {...this.props}></this.Mapper>;
+  }
+}
