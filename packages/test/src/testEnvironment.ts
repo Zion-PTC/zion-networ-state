634b4922df0882a5f66004cbe25395b8e9600226
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-21 17:08:44 +0100

    AAA - build @zionstate/test

--- packages/test/src/testEnvironment.ts
+++ packages/test/src/testEnvironment.ts
@@ -1,10 +1,7 @@
 import chai, { expect } from "chai";
 import promise from "chai-as-promised";
 import Mocha from "mocha";
-import { node } from "@zionstate/utils";
-
-const zionUtil = node.util.zionUtil.zionUtil;
-type DebugLogger = node.util.zionUtil.DebugLogger;
+import { debuglog, DebugLogger } from "util";
 
 chai.use(promise);
 
@@ -23,11 +20,11 @@ export const testEnvironment = (
   const testRunner = new Mocha({ slow: amount });
   testRunner.suite.emit("pre-require", global, "nofile", testRunner);
   var suiteRun = testRunner.run();
-  process.on("exit", (code) => {
+  process.on("exit", code => {
     // FIX not sure of this one
     process.exit(suiteRun.stats ? (suiteRun.stats.failures > 0 ? 1 : 0) : code);
   });
-  let log = zionUtil.debuglog("log");
+  let log = debuglog("log");
   return {
     expect,
     log,
