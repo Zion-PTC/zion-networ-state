51251594603658e5cdcba6bc9177939284564fb1
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-22 18:16:44 +0100

    + ui

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,8 @@
-// TODO add entrypoints for Next and React?
+// TODO #19 @giacomogagliano riordinare gli export
+// TODO #15 @giacomogagliano controllare gli export in HTML/REACT/classes
+// TODO #16 @giacomogagliano controllare gli export in HTML/REACT/lib
+// TODO #17 @giacomogagliano controllare gli export in HTML/REACT/style
+// TODO #18 @giacomogagliano controllare gli export in HTML/REACT/themes
 export * as HTML from "./HTML/index";
 export * as themes from "./HTML/React/themes";
 
@@ -7,3 +11,4 @@ export * as Colors from "./Colors";
 
 //// CLI
 export * from "./CLI";
+//
--- packages/ui/src/pages/classes/icon/index.tsx
+++ packages/ui/src/pages/classes/icon/index.tsx
@@ -4,7 +4,7 @@ export default function index() {
   // TODO #12 aggiungere link ad ogni icona
   // TODO mettere le icone in mezzo la pagina e a tutto
   // schermo, forse questo conviene farlo se possibile nel
-  // defaul delle icone
+  // defaul delle icone #
   //
   return <div>index</div>;
 }
