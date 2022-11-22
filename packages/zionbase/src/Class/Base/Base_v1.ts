2bcc1c93e4b6e5ffe2299b1cce766286467fe16f
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-22 18:20:57 +0100

    started new Base class in zionbase

--- packages/zionbase/src/Class/Base/Base_v1.ts
+++ packages/zionbase/src/Class/Base/Base_v1.ts
@@ -1,4 +1,24 @@
-import { staticImplements as si } from "../Decorators/staticImplements.js";
+import { staticImplements as si } from "../../Decorators/staticImplements.js";
+
+export interface IBase_v1 {
+  name: string;
+}
+
+export interface Base_v1 {
+  name: string;
+}
+
+export class Base_v1 implements IBase_v1 {
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+export type Base_v1Ctor = {
+  new (name: string): Base_v1;
+};
+
+export const Base_v1Ctor: Base_v1Ctor = Base_v1;
 
 const staticImplements = si;
 
