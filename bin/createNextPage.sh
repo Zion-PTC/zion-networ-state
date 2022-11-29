d3d5f4427551c551d295656891d8d64574a614d0
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-29 10:41:52 +0100

    added import React

--- bin/createNextPage.sh
+++ bin/createNextPage.sh
@@ -5,6 +5,8 @@ file=${page}/index.tsx
 
 mkdir ${page} &&
   touch ${file} &&
+  echo "import React from \"react\";" >>${file} &&
+  echo "" >>${file} &&
   echo "export default function index() {" >>${file} &&
   echo "  return (" >>${file} &&
   echo "    <div>index</div>" >>${file} &&
