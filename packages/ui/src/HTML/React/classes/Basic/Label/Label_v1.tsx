0e16c2866205515f350c5202fcf4d2cd3059cc6c
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-29 22:34:56 +0100

    added isMultiple in props and started styling

--- packages/ui/src/HTML/React/classes/Basic/Label/Label_v1.tsx
+++ packages/ui/src/HTML/React/classes/Basic/Label/Label_v1.tsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
-import { Input, InputAsChild } from "../Input";
+import {
+  MakeAsChild,
+  NoizDatas,
+  NoizProps,
+} from "../../../lib/types";
+import { Flatten } from "../../../lib/utility";
+import {
+  Input,
+  InputAsChild,
+  InputClassProps,
+  InputData,
+} from "../Input";
 
 /////// TYPES
-export type Label_v1Data = { name: string };
+export type Label_v1Data = Flatten<
+  { name: string } & InputData
+>;
 
 export type Label_v1Booleans = {};
 
@@ -10,7 +23,15 @@ export type Label_v1Props = NoizProps<
   Label_v1Data & Label_v1Booleans & InputAsChild
 >;
 
+export type Label_v1ClassBooleans = {
+  isMultiple?: boolean;
+};
+
 export type Label_v1ClassProps = NoizDatas<Label_v1Props>;
+
+export type Label_v1State = {
+  isMultiple: boolean;
+};
 
 export type Label_v1AsChild = MakeAsChild<
   "Label",
@@ -21,21 +42,59 @@ export type Label_v1AsChild = MakeAsChild<
 ////////CLASS
 export class Label_v1 extends BaseNoiz<
   Label_v1Data & InputAsChild,
-  Label_v1Booleans
+  Label_v1Booleans,
+  Label_v1State
 > {
   constructor(props: Label_v1ClassProps) {
     super(props);
+    if (this.props.datas.length === 1)
+      this.state = { isMultiple: false };
+    else this.state = { isMultiple: true };
   }
   Html = (props: Label_v1Props) => {
+    const {
+      handleChange,
+      placeholder,
+      type,
+      min,
+      max,
+      step,
+      preventDefault,
+    } = props;
+    const inputProps: InputClassProps = {
+      datas: [
+        {
+          handleChange,
+          placeholder,
+          type,
+          min,
+          max,
+          step,
+          preventDefault,
+        },
+      ],
+    };
     return (
-      <label className={props.className}>
-        <p>{props.name}</p>
-        <Input datas={props.Input} />
-      </label>
+      <>
+        <label className={props.className}>
+          <p>{props.name}</p>
+          <Input datas={inputProps.datas} />
+        </label>
+      </>
     );
   };
 
-  Style = styled(this.Html)``;
+  Style = styled(this.Html)`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    p {
+      display: inline;
+      margin-right: 0.3rem;
+    }
+    input {
+      display: inline;
+    }
+  `;
 
   Mapper = Label_v1.mapperFactory(this.Style);
 
