c1d92cd91d3308c5ee6ffa11d84646facd37b22f
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-29 10:43:30 +0100

    added comment

--- packages/utils/src/node/util/ZionUtils/guard/index.ts
+++ packages/utils/src/node/util/ZionUtils/guard/index.ts
@@ -1,4 +1,13 @@
-export {}
+export {};
 
 ///////// dataGuard
 export * from "./dataGuard";
+
+///////// keyInObjGuard
+export * from "./keyInObjGuard";
+
+///////// makeErrGuard
+export * from "./makeErrGuard";
+
+///////// optionGuard
+export * from "./optionGuard";
