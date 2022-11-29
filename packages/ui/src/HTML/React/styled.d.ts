b5a939e4db8495d790d6ccad0c16fa1e7e07c635
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-29 22:33:33 +0100

    fix

--- packages/ui/src/HTML/React/styled.d.ts
+++ packages/ui/src/HTML/React/styled.d.ts
@@ -1,6 +1,7 @@
 // import original module declarations
 import "styled-components";
 import {} from "styled-components/cssprop";
+import { FluidTheme } from "./lib/types/theme";
 
 // and extend them!
 declare module "styled-components" {
