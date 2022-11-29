9e6d88efb08b752dabbda53a7d2aeb212abc59e6
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-11-29 22:37:25 +0100

    +

--- packages/ui/src/pages/_app.tsx
+++ packages/ui/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FC } from "react";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../HTML/React/style/Global";
 import { lightTheme } from "../HTML/React/themes";
 
@@ -9,6 +9,19 @@ const theme = lightTheme;
 export async function getStaticProps() {
   return { props: { somethin: "true" } };
 }
+
+const bgcolor = `hsl(${210 + 90}, 55%, 75%)`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  header {
+    background-color: ${bgcolor};
+  }
+  footer {
+    background-color: ${bgcolor};
+  }
+`;
 
 export default function (props: {
   Component: FC;
@@ -19,7 +32,11 @@ export default function (props: {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps}></Component>
+      <Layout>
+        <header>I am the header</header>
+        <Component {...pageProps}></Component>
+        <footer>I am the footer</footer>
+      </Layout>
     </ThemeProvider>
   );
 }
