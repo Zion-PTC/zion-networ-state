561092ef7281ccf6d8ec8aae67fccbf35456d7d6
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-06 19:36:19 +0100

    new Processing noiz

--- packages/zionbase/src/Class/ProcessingNoiz/ProcessingNoiz_v1.ts
+++ packages/zionbase/src/Class/ProcessingNoiz/ProcessingNoiz_v1.ts
@@ -49,6 +49,47 @@ export interface IProcessingNoiz_v1 {
   name: string;
 }
 
+/**
+ * ```ts
+ * import {
+ *   functionality,
+ *   plugin,
+ *   ProcessingNoiz,
+ *   processor,
+ * } from "@zionstate/zionbase";
+ *
+ * type aType = { name: string };
+ * const blobProc: processor<aType> = mutator => a => {
+ *   a.name = a.name + mutator.name;
+ *   return a;
+ * };
+ *
+ * let blob = new ProcessingNoiz(blobProc);
+ *
+ * let avulc: plugin<aType>,
+ *   agirl: plugin<aType>,
+ *   scratched: plugin<aType>;
+ *
+ * const simpleProcessor: functionality<aType> = (
+ *   a: aType
+ * ) => {
+ *   return { name: a.name + "auch" };
+ * };
+ *
+ * avulc = blob.makePlugin({ name: "🌋" });
+ * agirl = blob.makePlugin({ name: "🧑" });
+ * scratched = blob.makePlugin(simpleProcessor);
+ *
+ * const res3 = blob
+ *   .use(avulc)
+ *   .use(agirl)
+ *   .use(scratched)
+ *   .process({ name: "santa-" });
+ * console.log(res3);
+ *
+ * // yelds { name: 'santa-🌋🧑auch' }
+ * ```
+ */
 export interface ProcessingNoiz_v1<
   T = unknown,
   F extends functionality<T> = functionality<T>,
