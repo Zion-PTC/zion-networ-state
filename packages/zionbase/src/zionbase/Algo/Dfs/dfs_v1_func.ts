a9c0f4d0603ec20dacd2d944dcf9b40bf902a077
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-11 20:34:53 +0100

    deleted old dfs function

--- packages/zionbase/src/zionbase/Algo/Dfs/dfs_v1_func.ts
+++ packages/zionbase/src/zionbase/Algo/Dfs/dfs_v1_func.ts
@@ -1,8 +1,9 @@
-import { BinaryTree } from "../../Graph/Tree/Trees";
-import { GraphTraverse } from "../../GraphTraverse";
-
+import { BinaryTree } from "../../DataStructures/Graph/Tree/Trees";
+// import { GraphTraverse } from "../../GraphTraverse";
+// @ts-expect-error
 export class DFS extends GraphTraverse {
   traverseDFS() {
+    // @ts-expect-error
     let stack: BinaryTree[] = [this.root];
     let res: string[] = [];
     while (stack.length) {
--- packages/zionbase/src/zionbase/DataStructures/lib/dfs/index.ts
+++
@@ -1,1 +0,0 @@
-export { DFS as dfs } from "./dfs_v1";
