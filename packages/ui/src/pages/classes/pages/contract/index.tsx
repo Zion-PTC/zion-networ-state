b00497e878782ec441a2c7fc4d0caf85c2d59efd
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-12 21:01:07 +0100

    little step forward

--- packages/ui/src/pages/classes/pages/contract/index.tsx
+++ packages/ui/src/pages/classes/pages/contract/index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { Contract } from "../../../../HTML/React";
 
 export default function index() {
-  return <Contract></Contract>;
+  return (
+    <Contract
+      price={Math.round((35 / 50) * 100) / 100}
+    ></Contract>
+  );
 }
