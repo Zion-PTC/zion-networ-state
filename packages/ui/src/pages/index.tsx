a2b23449a70bcc20df28c84f5c18c550a1a84821
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-12 15:10:16 +0100

    installed new dep "UA-parser" and made an issue
    + added issue

--- packages/ui/src/pages/index.tsx
+++ packages/ui/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import fs from "fs";
 import "./tesx";
 import IndexPage from "./components/IndexPage";
+import { UAParser } from "ua-parser-js";
 
 const makeDati = IndexPage.makeDati;
 const filter = IndexPage.filter;
@@ -14,6 +15,7 @@ const _document = filter("_document.tsx");
 
 export function getStaticProps() {
   const res = fs.readdirSync("./src/pages/");
+
   const filtered = res
     .filter(ds)
     .filter(noiz)
@@ -22,12 +24,18 @@ export function getStaticProps() {
     .filter(indexts)
     .filter(app);
 
-  const neww = filtered.map(makeDati);
-  // console.log(neww);
+  const data = filtered.map(makeDati);
 
-  return { props: { data: JSON.stringify(neww) } };
+  return { props: { data: JSON.stringify(data) } };
 }
 
 export default function index({ data }: { data: string }) {
+  // TODO #67 @ariannatnl ho aggiunto UaParser che ci permette
+  // di analizzar lo user agent. Ora recura il valore che ti
+  // da quando lo apri su edge e scrivilo qui... :
+  const name = new UAParser().getBrowser().name;
+  const version = new UAParser().getBrowser().version;
+  const major = new UAParser().getBrowser().major;
+  console.log(name, version, major);
   return <IndexPage data={data}></IndexPage>;
 }
