35161ea09ddc21c9636f424038f6e7e34c09a777
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-12 11:30:27 +0100

    Node and some tests

--- packages/zionbase/src/zionbase/DataStructures/Graph/Node/Node_v2.ts
+++ packages/zionbase/src/zionbase/DataStructures/Graph/Node/Node_v2.ts
@@ -1,51 +1,157 @@
-type EncodedId = string;
+// type EncodedId = string;
 
-type basicProps<T = unknown> = { node: EncodedId } & T;
-type connectType<T> = (props: basicProps) => T;
+// type basicProps<T = unknown> = { node: EncodedId } & T;
 
-type isConnectedType = (props: basicProps) => boolean;
+// type findAdjacentNodes = () => EncodedId[];
 
-type findAdjacentNodes = () => EncodedId[];
-
-export interface INode_v2 {
-  id: EncodedId;
-  edges: EncodedId[];
-  connect: connectType<this>;
-  isConnected: isConnectedType;
-  findAdjacentNodes: findAdjacentNodes;
+enum nodeTypes {
+  array_ids = "array_ids",
+  array_nodes = "array_nodes",
+  map = "map",
+  object = "object",
 }
 
-export interface Node_v2 {
-  id: EncodedId;
-  edges: EncodedId[];
+type nodeTypeTypes = keyof typeof nodeTypes;
+
+export interface ArrayNodeIdsProps<
+  I extends string | number | symbol = number,
+  V = unknown
+> {
+  type: nodeTypes.array_ids;
+  id: I;
+  children: number[];
+  value: V;
 }
 
-export interface Node_v2Props {
-  id: EncodedId;
+export interface ArrayNodesProps<
+  I extends string | number | symbol = number,
+  V = unknown
+> {
+  type: nodeTypes.array_nodes;
+  id: I;
+  children: ArrayNodeIdsProps<I>[];
+  value: V;
 }
 
-export class Node_v2 implements INode_v2 {
-  constructor(props: Node_v2Props) {
-    this.id = props.id;
-    this.edges = [];
+export interface MapNodeProps<
+  I extends string | number | symbol = number,
+  V = unknown
+> {
+  type: nodeTypes.map;
+  id: I;
+  children: Map<I, MapNodeProps<I>>;
+  value: V;
+}
+
+export interface ObjectNodeProps<
+  I extends string | number | symbol = number,
+  V = unknown
+> {
+  type: nodeTypes.object;
+  id: I;
+  children: { [props in I]: ObjectNodeProps<I> };
+  value: V;
+}
+export type Node_v2Props<
+  Id extends string | number | symbol,
+  V = unknown
+> =
+  | ObjectNodeProps<Id, V>
+  | ArrayNodeIdsProps<Id, V>
+  | ArrayNodesProps<Id, V>
+  | MapNodeProps<Id, V>;
+
+export interface INode_v2<
+  T extends nodeTypeTypes,
+  Id extends string | number | symbol,
+  V
+> {
+  type: T;
+  id: Id;
+  children:
+    | ArrayNodeIdsProps["id"][]
+    | { [props in Id]: ObjectNodeProps<Id> }
+    | Map<Id, MapNodeProps<Id>>;
+  value: V;
+  addChild: (n: number) => this;
+  hasChild: (n: number) => boolean;
+  hasChildren: () => boolean;
+  findAdjacentNodes: () => Id[];
+}
+
+export interface Node_v2<T extends nodeTypeTypes, Id, V> {
+  type: T;
+  id: Id;
+  children:
+    | ArrayNodeIdsProps["id"][]
+    | { [props in Id]: ObjectNodeProps<Id> }
+    | Map<Id, MapNodeProps<Id>>;
+  value: V;
+}
+
+export class Node_v2<
+  T extends nodeTypeTypes,
+  Id extends string | number | symbol,
+  V
+> implements INode_v2<T, Id, V>
+{
+  static nodeTypes = nodeTypes;
+  #NOT_IMPLEMENTED = "not yet implemented";
+  constructor(props: ArrayNodeIdsProps<Id>);
+  constructor(props: ObjectNodeProps<Id>);
+  constructor(props: MapNodeProps<Id>);
+  constructor(props: Node_v2Props<Id>) {
+    switch (props.type) {
+      case "array_ids":
+        this.children = props.children as number[];
+        this.id = props.id;
+        this.value = props.value as V;
+        break;
+      case "array_nodes":
+        throw new Error(this.#NOT_IMPLEMENTED);
+      case "map":
+        throw new Error(this.#NOT_IMPLEMENTED);
+      case "object":
+        throw new Error(this.#NOT_IMPLEMENTED);
+
+      default:
+        this.children = props.children as number[];
+        this.id = props.id;
+        this.value = props.value as V;
+        break;
+    }
   }
-  connect(props: basicProps) {
-    const { node } = props;
-    this.edges.push(node);
+  addChild(n: number) {
+    if (Array.isArray(this.children))
+      this.children.push(n as number);
+    else throw new Error("not yet implemented");
     return this;
   }
-  isConnected(props: basicProps) {
-    return this.edges.some(edge => edge === props.node)
-      ? true
-      : false;
+  hasChild(n: number) {
+    if (Array.isArray(this.children))
+      return this.children.some(edge => edge === n)
+        ? true
+        : false;
+    else throw new Error("not yet implemented");
   }
   findAdjacentNodes() {
-    return this.edges;
+    if (Array.isArray(this.children))
+      return this.children as Id[];
+    else throw new Error("not yet implemented");
   }
+  hasChildren: () => boolean = () => {
+    if (Array.isArray(this.children))
+      return this.children.length > 0;
+    else throw new Error("not yet implemented");
+  };
 }
 
-export type Node_v2Ctor = {
-  new (props: Node_v2Props): Node_v2;
+export type Node_v2Ctor<
+  T extends nodeTypeTypes = nodeTypes.array_ids,
+  Id extends string | number | symbol = string,
+  V = unknown
+> = {
+  new (props: ArrayNodeIdsProps<Id>): Node_v2<T, Id, V>;
 };
 
 export const Node_v2Ctor: Node_v2Ctor = Node_v2;
